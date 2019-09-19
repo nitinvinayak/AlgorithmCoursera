d0e43dc7f1304823bb3cd291328a17bcaf1ecea3
Nitin Vinayak Agrawal <f20170415@gmail.com>
2019-09-20 01:40:36 +0530

    3rd successful

--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd2.py
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd2.py
@@ -8,11 +8,12 @@ def gcd_naive(a, b):
     if (a>b):
     	max=a
     	min=b
-    while(max%min==0):
+    while(max%min!=0):
     	current_gcd=min
-    	min=max/min
+    	min=max%min
     	max=current_gcd
-    	
+    else:
+        current_gcd=min
 	
 
     return current_gcd
